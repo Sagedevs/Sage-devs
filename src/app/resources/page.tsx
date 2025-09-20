8d0f3c1e99b9561b0c9bc74fdf2b683b920b840f
Abdul Ahad <ahadking6152@gmail.com>
2025-09-19 19:49:04 -0700

    solving error

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -22,7 +22,7 @@ export default function ResourcesIndexPage() {
             <TechnologyStack />
             <WebinarsTalks />
             <ProjectCalculator />
-            <SupportCenter />
+          <SupportCenter />
       </main>
   );
 }
