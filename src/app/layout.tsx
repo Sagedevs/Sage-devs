921cf4af4c4554876751e71a6fb338d9feb5e3a8
Abdul Ahad <ahadking6152@gmail.com>
2025-09-26 12:28:58 -0700

    sole

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState, useCallback, useMemo } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { motion } from "framer-motion";
 import GooeyNavWithHeader from "@/blocks/Components/GooeyNav/GooeyNav";
 import { usePathname } from "next/navigation";
